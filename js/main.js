6b10fa89237bdb77f13eb76f74fdae612327ee19
hiratara <hiratara@cpan.org>
2010-12-28 14:23:41 +0900

    Copied the value of this to another variable.

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@
      };
      Room.prototype = {
          run : function () {
-            $.post(this.room_id + "/get_token", function (got) {
+            var room_id = this.room_id;
+
+            $.post(room_id + "/get_token", function (got) {
                 var channel = new goog.appengine.Channel(got.token);
                 var socket = channel.open();
                 socket.onopen = function () {
                     setInterval(function () {
-                        $.post(this.room_id + "/ping", {id : got.clientID}); 
+                        $.post(room_id + "/ping", {id : got.clientID}); 
                     }, 1000 * 60);
                 };
                 socket.onmessage = function (m) {
@@ -20,7 +22,7 @@
             }, "json");
 
             $("#say").submit(function (e) {
-                $.post(this.room_id + "/say", {saying : $("#saying").val()}); 
+                $.post(room_id + "/say", {saying : $("#saying").val()}); 
                 $("#saying").val('');
                 return false;
             });
